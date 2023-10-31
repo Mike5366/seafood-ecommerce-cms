b4953e815ec8f6f495c21cbd4cc4633df7a5ac75
Mike <mike2021us@gmail.com>
2023-10-31 14:40:02 -0700

    Update dropdown menu style

--- ecommerce-admin/app/(dashboard)/[storeId]/(routes)/billboards/components/cell-action.tsx
+++ ecommerce-admin/app/(dashboard)/[storeId]/(routes)/billboards/components/cell-action.tsx
@@ -65,7 +65,7 @@ export const CellAction: React.FC<CellActionProps> = ({ data }) => {
             <MoreHorizontal className="h-4 w-4" />
           </Button>
         </DropdownMenuTrigger>
-        <DropdownMenuContent align="end">
+        <DropdownMenuContent className="shadow-lg rounded-lg bg-white" align="end">
           <DropdownMenuLabel>Actions</DropdownMenuLabel>
           <DropdownMenuItem onClick={() => onCopy(data.id)}>
             <Copy className="mr-2 h-4 w-4" />
