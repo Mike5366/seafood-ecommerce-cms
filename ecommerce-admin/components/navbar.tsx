78154092e0005a740a5bbcf30a1a108d7c15adbb
Mike <mike2021us@gmail.com>
2023-12-27 09:29:11 -0800

    Complete dark mode

--- ecommerce-admin/components/navbar.tsx
+++ ecommerce-admin/components/navbar.tsx
@@ -1,29 +1,31 @@
 import { UserButton, auth } from "@clerk/nextjs";
 import { MainNav } from "@/components/main-nav";
 import StoreSwitcher from "@/components/store-switcher";
+import { ThemeToggle } from "@/components/theme-toggle";
 import { redirect } from "next/navigation";
 import prismadb from "@/lib/prismadb";
 
 const Navbar = async () => {
-  const {userId} = auth()
+  const { userId } = auth();
 
-  if(!userId){
-    redirect("/sign-in")
+  if (!userId) {
+    redirect("/sign-in");
   }
 
   const stores = await prismadb.store.findMany({
-    where:{
-      userId
+    where: {
+      userId,
     },
-  })
+  });
 
   return (
     <div className="border-b">
       <div className="flex h-16 items-center px-4">
-        <StoreSwitcher items={stores}/>
-        <MainNav className="mx-6"/>
+        <StoreSwitcher items={stores} />
+        <MainNav className="mx-6" />
         <div className="ml-auto flex items-center space-x-4">
-          <UserButton afterSignOutUrl="/"/>
+          <ThemeToggle />
+          <UserButton afterSignOutUrl="/" />
         </div>
       </div>
     </div>
